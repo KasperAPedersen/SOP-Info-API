c5add08d454825c46279e060acbe9d91b8fd34ed
Mads <Madsnellemann30@gmail.com>
2025-10-28 13:50:29 +0100

    d

--- routes/absenceRoute.js
+++ routes/absenceRoute.js
@@ -15,22 +15,16 @@ router.get('/:id/get', async (req, res) => {
 });
 
 router.post('/:id/set', async (req, res) => {
-    const { type, message } = req.body;
+    const id = parseInt(req.params.id);
+    const { status,message,type } = req.body;
     try {
-        const id = parseInt(req.params.id);
-        const absence = await models.Absence.findByPk(id);
-
-        if (!absence) {
-            return res.status(404).json({ error: "Absence not found" });
-        }
-
-        //add
         await models.Absence.create({
             userId: id,
             type: type,
-            ....
-        })
-        res.json(absence);
+            message: message,
+            status: status
+        });
+        res.status(201).json({ status: 'success' });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
