95e003c5462fbb41dbe23d2418a63a09931192d9
Kasper Doe <kasper@doe.dk>
2025-10-31 08:13:30 +0100

    fix: disable logging in Sequelize configuration for cleaner output

--- orm/database.js
+++ orm/database.js
@@ -6,7 +6,7 @@ dotenv.config();
 let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
     host: process.env.DB_HOST,
     dialect: process.env.DB_DIALECT,
-    lgging: false
+    logging: false
 });
 
 (async () => {
