eb9ba89b51e2741a6e3c2b164fde2ce3e276f104
Kasper Doe <kasper@doe.dk>
2025-11-03 08:52:48 +0100

    style(database): improve database connection log messages

--- orm/database.js
+++ orm/database.js
@@ -13,14 +13,14 @@ let sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.
     console.log('\n[INIT]\t\tInitializing...');
     try {
         await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
+        console.log('DB connection has been established successfully.');
     } catch (e) {
         console.error('Unable to connect to the database:', e);
     }
 
     try {
         await sequelize.sync();
-        console.log('Models has been synchronized.')
+        console.log('Sequelize models has been synchronized.')
     } catch (e) {
         console.error('Unable to synchronize models:', e);
     }
