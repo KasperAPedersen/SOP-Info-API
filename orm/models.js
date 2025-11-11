bd8ae670d951dd88e223c9545a3c5e7023f055d1
Kasper Doe <kasper@doe.dk>
2025-11-11 09:51:02 +0100

    refactor(models): standardize model import paths to lowercase

--- orm/models.js
+++ orm/models.js
@@ -1,6 +1,6 @@
-import User from './models/User.js';
-import Message from './models/Message.js';
-import Absence from './models/Absence.js';
+import User from './models/user.js';
+import Message from './models/message.js';
+import Absence from './models/absence.js';
 
 const models = {
     User,
